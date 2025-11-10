d8a4fafd2bf31ff1fdabd995eaaa6dee0516d775
Quang Pham <phamquang26805@gmail.com>
2025-11-10 23:14:42 +0700

    Add dashboard

--- services/finnhub_service.py
+++ services/finnhub_service.py
@@ -2,6 +2,7 @@ import json
 import websocket
 import requests
 
+from datetime import datetime
 from typing import List
 
 from services.kafka_service import KafkaService
@@ -20,6 +21,24 @@ class FinnhubService:
             if data["type"] == "ping":
                 return
             print(data)
+
+            now = datetime.now()
+            redis_api_host = "localhost"
+            redis_api_port = 8003
+
+            symbol = data["data"][0].get("s")
+            current_price = max([p.get("p") for p in data["data"]])
+            high_key = f"{now.strftime("%Y%m%d")}-{symbol}-high"
+            low_key = f"{now.strftime("%Y%m%d")}-{symbol}-low"
+            prev_close_key = f"{now.strftime("%Y%m%d")}-{symbol}-pc"
+
+            high_in_db = requests.get(f"http://{redis_api_host}:{redis_api_port}/q?key={high_key}")
+            low_in_db = requests.get(f"http://{redis_api_host}:{redis_api_port}/q?key={low_key}")
+            if high_in_db.status_code == 400 or float(high_in_db.json().get("value")) < current_price:
+                requests.post(f"http://{redis_api_host}:{redis_api_port}/q?key={high_key}&value={current_price}")
+            if low_in_db.status_code == 400 or float(low_in_db.json().get("value")) > current_price:
+                requests.post(f"http://{redis_api_host}:{redis_api_port}/q?key={low_key}&value={current_price}")
+
             serialized = json.dumps(data)
             self.kafka_service.send_message(
                 serialized, self.kafka_output_topic)
